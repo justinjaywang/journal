a16f794aab83f98facc8ad53da5eff15ea1005eb
Justin Wang <justinjaywang@gmail.com>
2015-08-11 10:22:57 -0400

    updated site 2015-08-11 10:22:57

--- js/all.js
+++ js/all.js
@@ -1,1 +1,35 @@
-+function(n){"use strict";var a="container container--narrow";n(".post-content").each(function(i,e){n(e).each(function(){n(this).removeClass(a)}),n(e).children().each(function(){n(this).addClass(a)}),n(e).find("img").each(function(a,i){var e=n(i).parent();e.hasClass("container")&&(e.find("br").remove(),n(i).unwrap()),n(i).wrap('<figure class="post-figure container container--wide"></figure>');var t=i.alt,r=n(i).parent();t&&r.append('<figcaption class="post-figure-caption">'+t+"</figcaption>")}),n(e).find("hr").each(function(){n(this).removeClass(a)})})}(jQuery);
++function ($) {
+  'use strict';
+
+  var narrowContainerClasses = 'container container--narrow';
+
+  $('.post-content').each(function (i, postContent) {
+    // (1) remove container classes from .post-content
+    $(postContent).each(function () {
+      $(this).removeClass(narrowContainerClasses);
+    });
+    // (2) add container--narrow to each of the children
+    $(postContent).children().each(function () {
+      $(this).addClass(narrowContainerClasses);
+    });
+    // (3) wrap images in a figure and append figcaption
+    $(postContent).find('img').each(function (j, image) {
+      var $imageWrap = $(image).parent();
+      if ($imageWrap.hasClass('container')) {
+        $imageWrap.find('br').remove(); // remove extra <br>
+        $(image).unwrap(); // remove .container wrap performed in (2)
+      }
+      $(image).wrap('<figure class="post-figure container container--wide"></figure>');
+      var alt = image.alt;
+      var $figure = $(image).parent();
+      if (alt) {
+        $figure.append('<figcaption class="post-figure-caption">' + alt + '</figcaption>');
+      }
+    });
+    // (4) remove container classes from hr
+    $(postContent).find('hr').each(function () {
+      $(this).removeClass(narrowContainerClasses)
+    });
+  });
+
+}(jQuery);
